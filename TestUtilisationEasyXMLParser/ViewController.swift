b13d835707aa7d15c24a9a4f562c6428dafc5741
PetitGrigri <fgriselles@hotmail.fr>
2017-02-15 12:23:02 +0100

    Mise à jour du controller permettant d'utiliser EasyXMLParser

--- TestUtilisationEasyXMLParser/ViewController.swift
+++ TestUtilisationEasyXMLParser/ViewController.swift
@@ -15,117 +15,89 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //utilisation d'un fichier
+        /*
+         * utilisation d'un fichier
+         */
         if let utilisateursFichier = Bundle.main.path(forResource: "utilisateurs", ofType: "xml") {
             if let utilisateursString = try? String(contentsOfFile: utilisateursFichier) {
                 if let utilisateurData = utilisateursString.data(using: .utf8) {
                     
-                    let parser = EasyXMLParser(withData: utilisateurData)
                     
+                    //parsing de notere fichier XML
+                    let parser = EasyXMLParser(withData: utilisateurData)
                     let items = parser.parse()
-                    let items2 = items.filter(filtre: ["utilisateur":["nom":"", "mail":""]])
+                    
 
                     
-                    for item in items2 {
-                        print("\n\n")
+                    //affichage des utilisateur "non filtré" via l'élément items
+                    print("   **********************************************")
+                    print("   *          UTILISATEURS NON FILTRES          *")
+                    print("   **********************************************")
+                    
+                    for item in items["utilisateurs"]["utilisateur"].getSiblingWithSameName() {
+                        print()
                         print(item.fullDescription())
-                        print("                             *                              ")
+                        print("                             -*-")
                     }
                     
-                    /*
-                    print(items);
-                    print(items.fullDescription())
-                    print("\n\nAffiche des données du premier utilisateur")
-                    print("____________________________________________________________")
+                    //affichage des utilisateurs fitré (sans parcours de items ou autre)
+                    let filtreUtilisateur = ["utilisateur":["nom":"", "mail":""]]
                     
+                    print("   **********************************************")
+                    print("   *          UTILISATEURS     FILTRES          *")
+                    print("   **********************************************")
                     
-                    let utilisateur1 = items["utilisateurs"]["utilisateur"].get()[0]
-                    print(utilisateur1.fullDescription())
-                    
-                    print("                             *                              ")
-                    
-                    print("\n\nAffiche de tout les utilisateurs du fichier utilisateur.xml")
-                    print("____________________________________________________________")
-                    
-                    print("Dans le fichier il y a \(items["utilisateurs"]["utilisateur"].count()) utilisateur")
-                    
-                    for item in items["utilisateurs"]["utilisateur"].get() {
-                        print("\n\n")
+                    for item in items.filter(filtre: filtreUtilisateur) {
+                        print()
                         print(item.fullDescription())
-                        print("                             *                              ")
-                        
-                    }*/
-                }
-            }
-        }
-        
-        /*
-        //version collection
-        if let url = URL.init(string: "https://korben.info/feed") {
-            if let xmlData = try? Data.init(contentsOf: url) {
-                
-                let parser = EasyXMLParser(withData: xmlData)
-                
-                //ne fonctonne pas pour le moment, mais ca va venir
-                /*let items = parser.fill(collection: [ "item" : ["title":"",
-                                                                "link":"",
-                                                                "category": []]] )*/
-                
-                //lancement de l'analyse complète du XML
-                let items = parser.parse()
-                
-                print ("\nAffichage du  résultat du fill (version collection) : ")
-                print("(filtre à venir) ")
-                
-                //affichage d'éléments précis de items (ici on regarde des informations concernant channel
-                print("\naffichage d'éléments précis : ")
-                print("____________________________________________________________")
-                print("['rss']['channel']['title'] : \(items["rss"]["channel"]["title"].value)")
-                print("['rss']['channel']['link'] : \(items["rss"]["channel"]["link"].value)")
-                
-                //affichage du dernier item du flux rss (c'est un comportement lié au subscrit de EasyXMLElement
-                print("\n\nAffichage du dernier item du flux rss")
-                print("____________________________________________________________")
-                print("['rss']['channel']['item'] : \(items["rss"]["channel"]["item"].name)")
-                
-                //affichage de tout les article de notre flux rss (correspondant aux balises item)
-                print("\n\nAffiche de tout les items")
-                print("____________________________________________________________")
-                print("nombre d'items : \(items["rss"]["channel"]["item"].count())")
-                for item in items["rss"]["channel"]["item"].get() {
-                    print("\n\n")
-                    print(item.fullDescription())
-                    print("                             *                              ")
+                        print("                             -*-")
+                    }
                     
+                    print("   **********************************************")
+                    print("   *       PREMIER UTILISATEUR CAST             *")
+                    print("   **********************************************")
+                    
+                    print("Code postal du premier utilisateur (int) : \(items["utilisateurs"]["utilisateur"]["code_postal"].intValue)")
+                    print("Code postal du premier utilisateur (float) : \(items["utilisateurs"]["utilisateur"]["code_postal"].floatValue)\n")
+
                 }
-                //[String:EasyXMLElement]
-                
-                
-                
             }
         }
-
-        */
         
-        //version objet
+        
         /*
+         *  Utilisation d'un URL
+         */
         if let url = URL.init(string: "https://korben.info/feed") {
             if let xmlData = try? Data.init(contentsOf: url) {
                 
-                print ("\nAffichage du  fill (version collection) : \n________________________________")
-                
+
                 let parser = EasyXMLParser(withData: xmlData)
+                let letFluxRSS = parser.parse()
                 
-                let premierItem = parser.fill(objectName: "TestUtilisationEasyXMLParser.Item").first as! Item
                 
-                print("Title : \(premierItem.title)")
-                print("Link : \(premierItem.link)")
-                print("comments : \(premierItem.comments)")
-                print("category : \(premierItem.category)")
+                print("   **********************************************")
+                print("   *         FLUX RSS   DONNES DU CHANNEL       *")
+                print("   **********************************************")
+
+                print("Nom du flux RSS : \(letFluxRSS["rss"]["channel"]["title"].value)")
+                print("Adress du flux RSS : \(letFluxRSS["rss"]["channel"]["link"].value)")
+                
+                print("Nombre d'article du flux RSS : \(letFluxRSS["rss"]["channel"]["item"].countSiblingWithSameName())")
+                print("Nombre de données pour le premier article : \(letFluxRSS["rss"]["channel"]["item"].countChildren()) \n\n")
+                
+                
+                print("   **********************************************")
+                print("   *         FLUX RSS          ARICLE           *")
+                print("   **********************************************")
+                
+                for article in letFluxRSS["rss"]["channel"]["item"].getSiblingWithSameName() {
+                    print()
+                    print(article.fullDescription())
+                    print("                             -*-")
+                }
             }
         }
-        */
-        
     }
     
     
